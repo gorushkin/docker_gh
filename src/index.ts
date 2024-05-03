5c99110a6c927d215c58db324ffedab24f4d4a03
gorushkin <gorushkin@gmail.com>
2024-05-03 21:06:40 +0300

    init commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+
+console.log('start');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send({
+    message: 'Hello World!',
+  });
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
